e1a10d9a748c917da38ad012799799121c4e56d1
Timothe Calvin <tcalv976@gmail.com>
2025-02-20 09:36:00 -0500

    added a 24/7 uptime for the bot

--- src/bots.js
+++ src/bots.js
@@ -1,64 +1,15 @@
-const Discord = require("discord.js");
+const { Client, IntentsBitField } = require('discord.js');
 
-const client = new Discord.Client({
-  intents: ["GUILDS", "GUILDS_MESSAGES", "GUILD_MEMBERS", "MESSAGE_CONTENT"],
-  partials: ["CHANNEL", "MESSAGE"],
+const myIntents = new IntentsBitField();
+myIntents.add(IntentsBitField.Flags.GuildPresences, IntentsBitField.Flags.GuildMembers);
+
+const client = new Client({ intents: myIntents });
+
+// other examples:
+const otherIntents = new IntentsBitField([IntentsBitField.Flags.Guilds, IntentsBitField.Flags.DirectMessages]);
+otherIntents.remove([IntentsBitField.Flags.DirectMessages]);
 
 
-});
-// remember to change bot token after uploading
-// token can be grabbed from bot creation under bot tag (use reset token to get code have to refresh it each time.)
-const token =
-  "";
-
-client.on("ready", async () => {
-  console.log(`client has been initiated! ${client.user.username}`);
-
-  client.guilds.cache.forEach((guild) => {
-    setupSlashCommands(client, guild.id);
-  });
-});
-
-client.on("messageCreate", async (message) => {
-  if (message.content.toLowerCase() === "test") {
-    message.reply("test successful").catch((err) => console.error(err));
-  }
-});
-// ensure to click bot and administrator when making discord bot
-function setupSlashCommands(client, guildId) {
-  const commands = [
-    {
-      name: "mimic",
-      description: "replies with whatever you said",
-      options: [
-        {
-          name: "say",
-          description: "the thing you want the bot to say",
-          type: "string",
-          required: true,
-          //   max length for bots code so discord and bot don't break
-          maxLength: 2000,
-        },
-      ],
-    },
-  ];
-
-  const guild = client.guilds.cache.get(guildId);
-  if (guild) {
-    console.error(`Guild with the ${guild} was not found.`);
-
-    guild.commands.set(commands).catch((error) => {
-      console.error(
-        `error setting commands for guild ${guild.name} (${guild.id})`,
-        error
-      );
-    });
-    client.on("interactionCreate", async (Interactions) => {
-      const mimic = Interactions.options.getString("say");
-
-      await Interactions.reply(`You said:\n${mimic}`);
-    });
-  }
-}
-
-client.login(token);
+client.login(
+  "MTM0MTc5MTA2MzMzMzE0Njc2NQ.Gjk0ma.QHboRTNEWTLBF-ZgzSq8bB-r743aiGpYlFUGEs"
+)
