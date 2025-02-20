317599e58b75fa8cdb775f739d977917eac1ea1f
Timothe Calvin <tcalv976@gmail.com>
2025-02-19 20:46:23 -0500

    fixing file maybe?

--- bots.js
+++ bots.js
@@ -4,3 +4,10 @@ const client = new Discord.Client({
     intents:["GUILDS" , "GUILDS_MESSAGES", "GUILD_MEMBERS", "MESSAGE_CONTENT"],
     partials:["CHANNEL", "MESSAGE"],
 })
+// remember to change bot token after uploading
+const token = ("")
+
+
+client.on('ready',async()=>{
+console.log(`client has been initiated! ${client.user.username}`);
+});
