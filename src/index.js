e1c249102bc1bcee0393d85d35b1fd371e40616b
Timothe Calvin <tcalv976@gmail.com>
2025-02-21 17:23:30 -0500

    turned the bot online and added a basic response to the bot

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-const {Client, IntentsBitField } = require('discord.js');
+const { TOKEN } = require('./config.json');
+const { Client, IntentsBitField } = require("discord.js");
 
 const client = new Client({
-	intents:
-	IntentsBitField.Flags.Guilds,
-	
-})
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on("ready", (c) => {
+  console.log(`${c.user.tag} is online.`);
+});
+client.on("messageCreate", (message) => {
+  if (message.author.bot) {
+    return;
+  }
+  console.log(message.content);
+  if ((message.content === "hello", "Hello")) {
+    message.reply("HEY!");
+  }
+});
+
+client.login(TOKEN);
+
